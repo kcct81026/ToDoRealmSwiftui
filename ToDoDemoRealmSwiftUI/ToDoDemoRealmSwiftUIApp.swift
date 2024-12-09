89ba6bee8863b1b2cbd03289cfd745ff3ab4fa97
KhinCho <khinchotint@onenex.co>
2024-12-09 22:02:21 +0630

    Initial Commit

---
+++ ToDoDemoRealmSwiftUI/ToDoDemoRealmSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoRealmSwiftUIApp.swift
+//  ToDoDemoRealmSwiftUI
+//
+//  Created by Kcct on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoRealmSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
